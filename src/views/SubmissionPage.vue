f407327c80902a59e91a3de65e05dbf91f164816
Patrick Steppan <patrick.steppan@gmail.com>
2019-02-18 21:41:38 -0600

    delay submission

--- src/views/SubmissionPage.vue
+++ src/views/SubmissionPage.vue
@@ -2,7 +2,7 @@
   <section class="w-100 h-100">
     <div class="w-75 w-50-ns h-100 fr flex flex-column justify-center">
     <h1 class="serif lh-display f3 near-black">
-      submissions open february 18th
+      submissions open february 19th
     </h1>
     <!-- <p class="sans-serif measure-narrow lh-copy mb3 near-black f6">
       please submit portable work in person. the submission table is in <span class="small-caps">wac</span> on the second floor near the open studios.
