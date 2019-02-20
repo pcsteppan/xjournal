51f806036b014f44cd4703e548560163d2463f14
Patrick Steppan <patrick.steppan@gmail.com>
2019-02-20 11:04:22 -0600

    open submission winter 2019

--- src/views/SubmissionPage.vue
+++ src/views/SubmissionPage.vue
@@ -2,20 +2,20 @@
   <section class="w-100 h-100">
     <div class="w-75 w-50-ns h-100 fr flex flex-column justify-center">
     <h1 class="serif lh-display f3 near-black">
-      submissions open february 19th
+      submit
     </h1>
-    <!-- <p class="sans-serif measure-narrow lh-copy mb3 near-black f6">
+    <p class="sans-serif measure-narrow lh-copy mb3 near-black f6">
       please submit portable work in person. the submission table is in <span class="small-caps">wac</span> on the second floor near the open studios.
     </p>
-    <p class="sans-serif measure-narrow lh-copy mb3 near-black f6">
+    <!-- <p class="sans-serif measure-narrow lh-copy mb3 near-black f6">
       request a photographer <a class="link red dim" href="https://goo.gl/forms/W5aYHklrM8o0HkHk2">here.</a>
-    </p>
-    <p class="sans-serif measure-narrow lh-copy mb3 near-black f6">
-      submit files <a class="link red dim" href="https://goo.gl/forms/3mLAWzYeLOPi4wBo2">here.</a>
-    </p>
-    <p class="sans-serif measure-narrow lh-copy mb3 near-black f6">
-      please send questions to <a class="link red" href = "mailto: xjournal@knox.edu">xjournal@knox.edu</a>
     </p> -->
+    <p class="sans-serif measure-narrow lh-copy mb3 near-black f6">
+      submit digital files <a class="link red dim" href="https://goo.gl/forms/3mLAWzYeLOPi4wBo2">here</a>.
+    </p>
+    <p class="sans-serif measure-narrow lh-copy mb3 near-black f6">
+      please send questions to <a class="link red" href = "mailto: xjournal@knox.edu">xjournal@knox.edu</a>.
+    </p>
   </div>
   </section>
 </template>
