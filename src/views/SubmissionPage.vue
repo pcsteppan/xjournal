5db59f3aec7ec10411866ef42b7b1181ca63e243
Patrick Steppan <patrick.steppan@gmail.com>
2019-11-12 14:34:53 -0600

    issue three submissions

--- src/views/SubmissionPage.vue
+++ src/views/SubmissionPage.vue
@@ -2,10 +2,38 @@
   <section class="w-100 h-100">
     <div class="w-75 w-50-ns h-100 fr flex flex-column justify-center">
     <h1 class="serif lh-display f3 near-black">
-      submissions closed
+      submissions for <span class="red">issue 3</span> are open.
     </h1>
     <p class="sans-serif measure-narrow lh-copy mb3 near-black f6">
-      submissions will be decided on and returned once they're documented.
+      physical work can be submitted in WAC, on the second floor, near the open studios.
+    </p>
+    <p class="sans-serif measure-narrow lh-copy mb3 near-black f6">
+      if your work is digital in nature, then please submit online here &#10230; <a class="link red" href="https://docs.google.com/forms/d/e/1FAIpQLSdB7aHCky1GelY_HCh0Rn11Id8mLB0T5-9oWqXEWIt2YBcvMA/viewform?usp=sf_link">form</a>
+    </p>
+    <!-- <p class="sans-serif measure-narrow lh-copy near-black f6 b">we accept:</p> -->
+    <!-- <ul style="list-style: outside circle; column-count: 2;" class="sans-serif measure-narrow lh-solid mb3 near-black f6">
+      <li>animation</li>
+      <li>ceramics</li>
+      <li>costumes</li>
+      <li>dance</li>
+      <li>graphic design</li>
+      <li>drawing</li>
+      <li>illustration</li>
+      <li>mixed media</li>
+      <li>new media</li>
+      <li>painting</li>
+      <li>performance</li>
+      <li>printmaking</li>
+      <li>photography</li>
+      <li>sculpture</li>
+      <li>set design</li>
+      <li>textiles</li>
+      <li>typography</li>
+      <li>video</li>
+    </ul>
+    <p class="sans-serif measure-narrow lh-copy mb3 near-black f6">does your work not fit any of that? if your work is visual, we'd like to see it.</p> -->
+    <p class="sans-serif measure-narrow lh-copy mb3 near-black f6">
+      issue 3 will be printed and released in the spring.
     </p>
     <p class="sans-serif measure-narrow lh-copy mb3 near-black f6">
       please send questions to <a class="link red" href = "mailto: xjournal@knox.edu">xjournal@knox.edu</a>.
