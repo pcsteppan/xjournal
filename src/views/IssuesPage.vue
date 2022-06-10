9d73e33dbba1fa718eba5bd4f9d1448a7e51ad7e
DanTheBriggs <dtbrigg@knox.edu>
2022-06-09 21:10:08 -0500

    fourth issue displaying

--- src/views/IssuesPage.vue
+++ src/views/IssuesPage.vue
@@ -42,6 +42,22 @@
           3
         </p>
       </div>
+
+      <!-- Change colors of text here                                   V   -->
+      <router-link class="b pl2 pl4-ns link f1 sans-serif bw1 b--solid riso-burgundy b--riso-burgundy br-0 bb-0" id="issue-4-essay" to="/issue/4/essay">
+        fourth
+      </router-link>
+      <router-link class="b pl2 pl4-ns link f1 sans-serif bw1 b--solid riso-burgundy b--riso-burgundy br-0 bb-0" id="issue-4-artworks" to="/issue/4/artwork">
+        artwork
+      </router-link>
+      <router-link class="b pl2 pl4-ns link f1 sans-serif bw1 b--solid riso-burgundy b--riso-burgundy bb-0" id="issue-4-artists" to="/issue/4/artists">
+        artists
+      </router-link>
+      <div id="link-wrapper-4" class="flex items-center h-100">
+        <p class="tc f-banner f-banner-l f-banner-m display riso-burgundy h-100 center">
+          4
+        </p>
+      </div>
     </section>
 </template>
 
@@ -57,6 +73,10 @@
   #link-wrapper-3{
     background: linear-gradient(90deg, var(--riso-kellygreen), rgba(0,0,0,0) 30%);
     grid-area: iss3;
+  }
+  #link-wrapper-4{
+    background: linear-gradient(90deg, var(--riso-burgundy), rgba(0,0,0,0) 30%);
+    grid-area: iss4;
   }
   #issue-1-essay{
     grid-area: e1;
@@ -85,6 +105,17 @@
   #issue-3-artists{
     grid-area: ppl3;
   }
+
+  #issue-4-poem{
+    grid-area: p4;
+  }
+  #issue-4-artworks{
+    grid-area: work4;
+  }
+  #issue-4-artists{
+    grid-area: ppl4;
+  }
+
   #issue-nav-wrapper{
     top: 0;
     display: grid;
@@ -103,7 +134,10 @@
       "iss2 ppl2"
       "iss3 p3"
       "iss3 work3"
-      "iss3 ppl3";
+      "iss3 ppl3"
+      "iss4 p4"
+      "iss4 work4"
+      "iss4 ppl4";
   }
   @media screen and (min-width: 60rem){
     #link-wrapper-1{
@@ -120,6 +154,17 @@
     }
     #link-wrapper-3{
       background: linear-gradient(90deg, var(--riso-kellygreen), rgba(240,240,240,0) 30%);
+      /* grid-column-start: 4;
+      grid-row: 1/4; */
+      /* writing-mode: vertical-lr; */
+    }
+    
+    
+    /*change background color here */
+
+
+    #link-wrapper-4{
+      background: linear-gradient(90deg, var(--riso-burgundy), rgba(240,240,240,0) 30%);
       /* grid-column-start: 4;
       grid-row: 1/4; */
       /* writing-mode: vertical-lr; */
@@ -154,13 +199,14 @@
       grid-auto-flow: column;
       width: 100%;
       height: 100%;
-      grid-template-columns: 1fr 1fr 1fr 1fr;
+      grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
       /* grid-template-rows: 1fr auto 1fr; */
-      grid-template-rows: 1fr 1fr 1fr;
+      grid-template-rows: 1fr 1fr 1fr 1fr;
       grid-template-areas:
       "iss1   e1    work1 ppl1"
       "iss2   e2    work2 ppl2"
-      "iss3   p3    work3 ppl3";
+      "iss3   p3    work3 ppl3"
+      "iss4   p4    work4 ppl4";
     }
   }
 </style>
