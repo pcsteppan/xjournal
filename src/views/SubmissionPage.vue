bcb69dab1f537ed096df8ebbcc97f4dc481c2ded
An <atrieu@knox.edu>
2022-11-06 13:25:38 -0600

    open up submission for issue 5

--- src/views/SubmissionPage.vue
+++ src/views/SubmissionPage.vue
@@ -1,46 +1,60 @@
 <template>
   <section class="w-100 h-100">
     <div class="w-75 w-50-ns h-100 fr flex flex-column justify-center pr3 pa0-ns">
-    <h1 class="serif lh-display f2 near-black mb3">
+      <!-- <h1 class="serif lh-display f2 near-black mb3">
       submissions for <span class="riso-burgundy">issue 4</span> are now closed.
-    </h1>
-    <!-- <p class="sans-serif measure-narrow lh-copy mb3 near-black f5 f4-ns">
-      physical work can be submitted in WAC, on the second floor, near the open studios.
-    </p>
-    <p class="sans-serif measure-narrow lh-copy mb3 near-black f5 f4-ns">
-      to submit online, please review our <router-link class="riso-kellygreen" to="./submit/guide">guide for documenting your own work</router-link> and then submit your work &#10230; <a class="riso-kellygreen" target="_blank" href="https://docs.google.com/forms/d/e/1FAIpQLSf63kCDd6cZJ_FBJHrBcz46tYQdSIg7NnSLkf8VuGUe1swllA/viewform?usp=sf_link">here.</a>
-    </p>
-    <p class="sans-serif measure-narrow lh-copy near-black f6 b">we accept:</p> -->
-    <!-- <ul style="list-style: outside circle; column-count: 2;" class="sans-serif measure-narrow lh-solid mb3 near-black f6">
-      <li>animation</li>
-      <li>ceramics</li>
-      <li>costumes</li>
-      <li>dance</li>
-      <li>graphic design</li>
-      <li>drawing</li>
-      <li>illustration</li>
-      <li>mixed media</li>
-      <li>new media</li>
-      <li>painting</li>
-      <li>performance</li>
-      <li>printmaking</li>
-      <li>photography</li>
-      <li>sculpture</li>
-      <li>set design</li>
-      <li>textiles</li>
-      <li>typography</li>
-      <li>video</li>
-    </ul>
-    <p class="sans-serif measure-narrow lh-copy mb3 near-black f6">does your work not fit any of that? if your work is visual, we'd like to see it.</p> -->
-    <!-- <p class="sans-serif lh-copy mb3 near-black f5 f4-ns">
-      submissions will close on the 28th, 12AM CDT
-    </p> -->
-    <!-- <p class="sans-serif lh-copy mb3 near-black f5 f4-ns">
-      issue 3 will be released online on the 29th.
-    </p> -->
-    <p class="sans-serif measure-narrow lh-copy mb3 near-black f5 f4-ns">
-      please send questions to <a class="link riso-burgundy" href = "mailto: xjournal@knox.edu">xjournal@knox.edu</a>.
-    </p>
-  </div>
+    </h1> -->
+      <p class="sans-serif measure-narrow lh-copy mb3 near-black f5 f4-ns">
+        physical work can be submitted in WAC, on the second floor, near the open studios.
+      </p>
+      <p class="sans-serif measure-narrow lh-copy mb3 near-black f5 f4-ns">
+        only submit your work here here if it is a digital work, or high resolution photo of your work in correct
+        format.
+        <br />
+        <em>
+
+          (We will have people take photo of your physical work).
+        </em>
+      </p>
+      <p class="sans-serif measure-narrow lh-copy mb3 near-black f5 f4-ns">
+        to submit online, please review our <router-link class="riso-kellygreen" to="./submit/guide">guide for
+          documenting your own work</router-link> and then submit your work &#10230; <a class="riso-kellygreen"
+          target="_blank"
+          href="https://docs.google.com/forms/d/e/1FAIpQLSe8JhtU_zU6ny1Q56ZYvyoXfnP_KNGYulVbOfSVyW6dO201IA/viewform?usp=pp_url">here</a>.
+      </p>
+      <p class="sans-serif measure-narrow lh-copy near-black f6 b">we accept:</p>
+      <ul style="list-style: outside circle; column-count: 2;"
+        class="sans-serif measure-narrow lh-solid mb3 near-black f6">
+        <li>animation</li>
+        <li>ceramics</li>
+        <li>costumes</li>
+        <li>dance</li>
+        <li>graphic design</li>
+        <li>drawing</li>
+        <li>illustration</li>
+        <li>mixed media</li>
+        <li>new media</li>
+        <li>painting</li>
+        <li>performance</li>
+        <li>printmaking</li>
+        <li>photography</li>
+        <li>sculpture</li>
+        <li>set design</li>
+        <li>textiles</li>
+        <li>typography</li>
+        <li>video</li>
+      </ul>
+      <p class="sans-serif measure-narrow lh-copy mb3 near-black f6">does your work not fit any of that? if your work is
+        visual, we'd like to see it.</p>
+      <p class="sans-serif lh-copy mb3 near-black f5 f4-ns">
+        submissions will close on Spring Term 2023.
+      </p>
+      <p class="sans-serif lh-copy mb3 near-black f5 f4-ns">
+        issue 5 will be released online on the April 8, 2023.
+      </p>
+      <p class="sans-serif measure-narrow lh-copy mb3 near-black f5 f4-ns">
+        please send questions to <a class="link riso-burgundy" href="mailto: xjournal@knox.edu">xjournal@knox.edu</a>.
+      </p>
+    </div>
   </section>
 </template>
