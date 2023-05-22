8c4345105716037097e4afbdc3f6a0dfd180dfa0
An <atrieu@knox.edu>
2023-05-21 19:57:14 -0500

    uploaded to page 35

--- src/components/Essay.vue
+++ src/components/Essay.vue
@@ -9,7 +9,7 @@
       <div id="essay--body-wrapper" class="w-100-ns lh-copy sans-serif f4 f3-ns pb3 near-black">
         <p v-for="(p, i) in this.essayData.paragraphs" :key="i" class="measure pb4">
           <b>{{p.split("\n")[1]}}</b>
-          <br/>
+          <br v-if="p.split('\n')[1]"/>
           {{p.split("\n")[0]}}
         </p>
       </div>
